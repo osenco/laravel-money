5d8f254d86f9014fc655be8d41e93572189f4f3b
Mauko <maukoese@gmail.com>
2024-12-04 02:37:52 +0300

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -4,7 +4,7 @@ use Akaunting\Money\Currency;
 use Akaunting\Money\Money;
 
 if (! function_exists('money')) {
-    function money(mixed $amount, string | null $currency = null, bool $convert = null): Money
+    function money(mixed $amount, string | null $currency = null, bool | null $convert = null): Money
     {
         if (is_null($currency)) {
             /** @var string $currency */
